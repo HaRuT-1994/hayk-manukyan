ac5440cf271f20e9bcb7dcb1ba8e053cd16592d2
Harut Manukyan <harut.manukyan@decript.tech>
2023-03-28 11:44:13 +0400

    start design to main page

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './components/landing/landing.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    LandingComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [HeaderComponent, LandingComponent]
+})
+export class CoreModule { }
